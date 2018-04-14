b2c4bd8719b2facaff7842dfd741993c02db33f2
Mohamed Amin Kedir <Amin@Mohameds-MacBook-Pro.local>
2018-04-14 07:44:04 -0500

    heroku final push

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
 
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
@@ -17,7 +25,44 @@ class App extends Component {
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
   };
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
 
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Fail Fail Fail",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    } else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
